c270fcf92e03f642c6e377e542e7282a62d16a07
fatih-demircan <fatihd.contact@gmail.com>
2025-08-20 19:05:58 +0200

    added tailwind

--- source/src/main.rs
+++ source/src/main.rs
@@ -4,6 +4,6 @@ use leptos_meta::provide_meta_context;
 fn main() {
     leptos::mount::mount_to_body(|| {
         provide_meta_context();
-        view! { <div>"Test"</div> }
+        view! { <div class="bg-red-300">"Test"</div> }
     })
 }
