54c4502d78e71260f5bd9d119ad420163b07ab7d
fatih-demircan <fatihd.contact@gmail.com>
2025-08-20 22:41:39 +0200

    added daisyui

--- source/src/main.rs
+++ source/src/main.rs
@@ -4,6 +4,9 @@ use leptos_meta::provide_meta_context;
 fn main() {
     leptos::mount::mount_to_body(|| {
         provide_meta_context();
-        view! { <div class="bg-red-300">"Test"</div> }
+        view! {
+            <div class="bg-red-300">"Test"</div>
+            <button class="btn btn-primary btn-soft">Test</button>
+        }
     })
 }
