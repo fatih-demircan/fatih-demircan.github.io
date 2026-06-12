5de46a0d48f924ebefa9d908e615f8e1dcabd4b8
fatih-demircan <fatihd.contact@gmail.com>
2026-06-13 00:50:44 +0200

    added contact address

--- source/src/app/contact.rs
+++ source/src/app/contact.rs
@@ -5,7 +5,7 @@ pub fn Contact() -> impl IntoView {
     view! {
         <Card title="Contact".to_string()>
             <div class="pt-6 text-justify indent-6 text-base-content/70">
-                "In case you have interesting projects or potential business inquiries, feel free to contact me. Contact details will be provided very soon."
+                "fatihdmrcn@outlook.com"
             </div>
         </Card>
     }
